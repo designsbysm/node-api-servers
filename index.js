5870b3e69d93db41b3cd3df238f76f68cf2acdc6
Scott A. Matthews <scott@designsbysm.com>
2019-10-23 14:03:46 -0700

    add .env config

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const errorHandler = require("./tools/errorHandler");
+const dotenv = require("dotenv");
 const express = require("express");
 const routes = require("./routes");
+
+dotenv.config();
+const port = process.env.PORT;
 
 const app = express();
 app.use(express.json({ limit: "10mb" }));
 app.use(routes);
 app.use(errorHandler);
-
-const port = 8080;
 
 app.listen(port, () => console.info("server listening on %s", port));
